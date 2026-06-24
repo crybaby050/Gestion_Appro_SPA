6db8253931e013b84049da3bb6cfdfc25025b2b0
crybaby05 <benthiam750@gmail.com>
2026-06-24 23:05:31 +0000

    mise a jour de api en ajoutant de nouveau endpoint

--- js/config/api.js
+++ js/config/api.js
@@ -1,6 +1,8 @@
-export const API_BASE_URL = "https://back-gestion-appro.onrender.com"
+export const API_BASE_URL = "https://back-gestion-appro.onrender.com";
 
 export const ENDPOINTS = {
-    categories: `${API_BASE_URL}/categories`,
-    products: `${API_BASE_URL}/products`,
-}
+  categories:   `${API_BASE_URL}/categories`,
+  products:     `${API_BASE_URL}/products`,
+  users:        `${API_BASE_URL}/users`,
+  fournisseurs: `${API_BASE_URL}/fournisseurs`,
+};
